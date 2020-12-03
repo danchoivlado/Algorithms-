4188571cc2e41143bea878f295d26d59c67e41e5
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-03 15:28:34 +0200

    _03._Generating_Vectors

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/01. Recursion and backtracking/Recursion and backtracking - Lab/03. Generating Vectors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03._Generating_Vectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Generate(new int[n]);
+        }
+
+        public static void Generate(int[] arr, int value = 0)
+        {
+            if (value == arr.Length)
+            {
+                Console.WriteLine(string.Join("", arr));
+                return;
+            }
+
+            for (int i = 0; i <= 1; i++)
+            {
+                arr[value] = i;
+                Generate(arr, value + 1);
+            }
+        }
+    }
+}
