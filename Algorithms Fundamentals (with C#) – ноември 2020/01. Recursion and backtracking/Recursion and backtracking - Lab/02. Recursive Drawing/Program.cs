63d1b96ac1572e1eb521faec2426bce04de74955
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-03 14:57:50 +0200

    Task 2

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/01. Recursion and backtracking/Recursion and backtracking - Lab/02. Recursive Drawing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02._Recursive_Drawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Draw(n);
+        }
+
+        public static void Draw(int n)
+        {
+            if(n == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new String('*', n));
+            Draw(n - 1);
+            Console.WriteLine(new String('#', n));
+        }
+    }
+}
