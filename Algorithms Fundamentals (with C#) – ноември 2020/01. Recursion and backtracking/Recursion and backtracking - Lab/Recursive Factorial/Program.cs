4c5b47b9d3690b4692185a8f3d6fa9dd083bf321
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-03 15:39:43 +0200

    Recursive Factorial

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/01. Recursion and backtracking/Recursion and backtracking - Lab/Recursive Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fib(n));
+        }
+
+        public static long Fib(int number)
+        {
+            if(number == 1)
+            {
+                return 1;
+            }
+
+            return number * Fib(number - 1);
+        }
+
+    }
+}
