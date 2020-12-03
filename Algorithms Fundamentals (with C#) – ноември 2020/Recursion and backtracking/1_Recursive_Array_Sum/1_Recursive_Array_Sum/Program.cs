f6b5bd7794d4965b19daf50aa2c0e1c33e11f277
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-03 14:11:07 +0200

    Task 1 initial

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/Recursion and backtracking/1_Recursive_Array_Sum/1_Recursive_Array_Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_Recursive_Array_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
