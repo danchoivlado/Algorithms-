d8756143d00db37d5eee22b6be9da655f59153b9
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-11 15:18:15 +0200

    Variations With Repetitions

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/02. Combinatorial Problems/Combinatorial Problems/04. Variations W Rep/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Variations_W_Rep
+{
+    class Program
+    {
+        private static char[] variations;
+        private static char[] elements;
+
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split().Select(x => char.Parse(x)).ToArray();
+            var k = int.Parse(Console.ReadLine());
+            variations = new char[k];
+
+            Generate(0);
+        }
+
+        private static void Generate(int index)
+        {
+            if(index >= variations.Length)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                variations[index] = elements[i];
+                Generate(index+1);
+            }
+        }
+    }
+}
