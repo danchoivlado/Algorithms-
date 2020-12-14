2c5cec9159ea446f4d280bfd4d4672edc716a89b
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-14 14:49:04 +0200

    Combinations WO Rep

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/03. Exercise Rec and Comb/Exercise Rec and Comb/04. Comb WO Rep/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _04._Comb_WO_Rep
+{
+    class Program
+    {
+        static int[] slots;
+        static void Main(string[] args)
+        {
+            var numElements = int.Parse(Console.ReadLine());
+            var numSlots = int.Parse(Console.ReadLine());
+            slots = new int[numSlots];
+
+            Generate(numElements);
+        }
+
+        private static void Generate(int numElements, int index = 0, int element = 1)
+        {
+            if(index >= slots.Length)
+            {
+                Console.WriteLine(string.Join(" ", slots));
+                return;
+            }
+
+            for (int i = element; i <= numElements; i++)
+            {
+                slots[index] = i;
+                Generate(numElements, index + 1, i + 1);
+            }
+        }
+    }
+}
