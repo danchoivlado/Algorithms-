7057cb1bada6af4b87a2866d1b2de13ce8dc0862
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-14 13:37:13 +0200

    02. Nesed Loops

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/03. Exercise Rec and Comb/Exercise Rec and Comb/02. Nested Loops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02._Nested_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var variations = new int[n];
+
+            Generate(variations, 0);
+        }
+
+        private static void Generate(int[] variations, int index)
+        {
+            if(index >= variations.Length)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+                return;
+            }
+
+
+            for (int i = 0; i < variations.Length; i++)
+            {
+                variations[index] = i+1;
+                Generate(variations, index + 1);
+            }
+        }
+    }
+}
