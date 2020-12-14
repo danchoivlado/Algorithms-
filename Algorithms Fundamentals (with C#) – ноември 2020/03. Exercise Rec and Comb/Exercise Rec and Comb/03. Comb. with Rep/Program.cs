3d2ce92aacc46accea011a444ddfa40362ddaaea
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-14 14:41:20 +0200

    03. Comb with Rep

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/03. Exercise Rec and Comb/Exercise Rec and Comb/03. Comb. with Rep/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Comb._with_Rep
+{
+    class Program
+    {
+        static int[] slots;
+
+        static void Main(string[] args)
+        {
+            var numElements = int.Parse(Console.ReadLine());
+            var numSLots = int.Parse(Console.ReadLine());
+            slots = new int[numSLots];
+
+            Print(numElements);
+        }
+
+        private static void Print(int numElements, int index = 0, int element = 1)
+        {
+            if (index >= slots.Length)
+            {
+                Console.WriteLine(string.Join(" ", slots));
+                return;
+            }
+
+            for (int i = element; i <= numElements; i++)
+            {
+                slots[index] = i;
+                Print(numElements, index + 1, i);
+            }
+        }
+    }
+}
