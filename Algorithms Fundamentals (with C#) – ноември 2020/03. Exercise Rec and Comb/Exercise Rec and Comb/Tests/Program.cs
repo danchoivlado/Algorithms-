4cb0add4e5280490199d51ab01e80a8e247ac899
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-16 13:16:33 +0200

    Test

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/03. Exercise Rec and Comb/Exercise Rec and Comb/Tests/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tests
+{
+    class Program
+    {
+        private static char[] elements;
+        private static char[] slots;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split().Select(char.Parse).ToArray();
+            var numSlots = int.Parse(Console.ReadLine());
+            slots = new char[numSlots];
+
+            Generate(0, 0);
+        }
+
+        private static void Generate(int index, int start)
+        {
+            if(index >= slots.Length)
+            {
+                Console.WriteLine(string.Join(" ", slots));
+                return;
+            }
+
+
+            for (int i = start; i < elements.Length; i++)
+            {
+                slots[index] = elements[i];
+                Generate(index + 1, i);
+            }
+        }
+    }
+}
