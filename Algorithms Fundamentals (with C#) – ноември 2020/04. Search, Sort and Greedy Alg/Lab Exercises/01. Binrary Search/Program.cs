29c2efebec9e0181bc266f09664a8f4c985c244e
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-17 17:09:03 +0200

    Binary Search

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/04. Search, Sort and Greedy Alg/Lab Exercises/01. Binrary Search/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Binrary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var searchedElements = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinarySearch(elements, searchedElements));
+        }
+
+        private static int BinarySearch(int[] elements, int searchedElements)
+        {
+            var left = 0;
+            var right = elements.Length - 1;
+            var middle = (left + right) / 2;
+
+            while (left <= right)
+            {
+                if (elements[middle] == searchedElements)
+                    return middle;
+
+                if (elements[middle] < searchedElements)
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+                middle = (left + right) / 2;
+            }
+            return -1;
+        }
+    }
+}
