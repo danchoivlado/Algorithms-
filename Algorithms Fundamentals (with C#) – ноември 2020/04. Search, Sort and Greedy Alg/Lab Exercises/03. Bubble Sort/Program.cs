6de02fc36b6fdccf4a8affceddc295747b7b5853
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-17 17:39:17 +0200

    Bubble sort

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/04. Search, Sort and Greedy Alg/Lab Exercises/03. Bubble Sort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Bubble_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            BubbleSort(elements);
+            Console.WriteLine(string.Join(" ", elements));
+        }
+
+        private static void BubbleSort(int[] elements)
+        {
+            var sorted = false;
+            while (sorted != true)
+            {
+                sorted = true;
+
+                for (int i = 0; i < elements.Length-1; i++)
+                {
+                    if(elements[i] > elements[i + 1])
+                    {
+                        Swap(elements, i, i + 1);
+                        sorted = false;
+                    }
+                }
+            }
+        }
+
+        private static void Swap(int[] elements, int first, int second)
+        {
+            var temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
+        }
+    }
+}
