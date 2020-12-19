1f76070f5ad2fa84ab6f961d86b76954093aef86
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-19 17:32:36 +0200

    DFS

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/05. Graph Theory/05. Graphs Lab/00. DFS/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _00._DFS
+{
+    class Program
+    {
+        public static HashSet<int> visitedNodes;
+        public static Dictionary<int, List<int>> graph;
+
+        static void Main(string[] args)
+        {
+            visitedNodes = new HashSet<int>();
+            graph = new Dictionary<int, List<int>>()
+            {
+                { 1, new List<int>(){19, 21, 14} },
+                { 19, new List<int>(){7, 12, 31, 21} },
+                { 21, new List<int>(){14} },
+                { 14, new List<int>(){23, 6} },
+                { 7, new List<int>(){1} },
+                { 12, new List<int>() },
+                { 31, new List<int>(){21} },
+                { 23, new List<int>(){21} },
+                { 6, new List<int>() },
+            };
+
+            foreach (var node in graph.Keys)
+            {
+              
+                    DFS(node);
+                
+            }
+        }
+
+        private static void DFS(int startNode)
+        {
+            if (visitedNodes.Contains(startNode))
+            {
+                return;
+            }
+
+            visitedNodes.Add(startNode);
+            foreach (var node in graph[startNode])
+            {
+                DFS(node);
+            }
+
+            Console.WriteLine(startNode);
+        }
+    }
+}
