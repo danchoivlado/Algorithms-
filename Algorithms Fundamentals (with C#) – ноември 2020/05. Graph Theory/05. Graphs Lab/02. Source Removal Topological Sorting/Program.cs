d67133dbbe229c8bc62f804a7a78ee66c06cee45
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-20 12:59:25 +0200

    Topological Sorting

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/05. Graph Theory/05. Graphs Lab/02. Source Removal Topological Sorting/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02._Source_Removal_Topological_Sorting
+{
+    class Program
+    {
+        public static Dictionary<string, List<string>> graph;
+        public static Dictionary<string, int> dependancies;
+
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            graph = ReadGraph(n);
+            dependancies = SortGraph();
+            var IsInvalid = false;
+            var res = GenRes(ref IsInvalid);
+
+            if (IsInvalid)
+            {
+                Console.WriteLine("Invalid topological sorting");
+                return;
+            }
+
+            Console.WriteLine($"Topological sorting: {string.Join(", ",res)}");
+
+        }
+
+        private static List<string> GenRes(ref bool isInvalid)
+        {
+            var lis = new List<string>();
+
+            while (dependancies.Count > 0)
+            {
+                var nodeDelete = dependancies.FirstOrDefault(x => x.Value == 0);
+
+
+                if (string.IsNullOrEmpty(nodeDelete.Key))
+                {
+                    isInvalid = true;
+                    break;
+                }
+
+                foreach (var child in graph[nodeDelete.Key])
+                {
+                    dependancies[child]--;
+                }
+
+                dependancies.Remove(nodeDelete.Key);
+                lis.Add(nodeDelete.Key);
+            }
+
+            return lis;
+        }
+
+        private static Dictionary<string, int> SortGraph()
+        {
+            var result = new Dictionary<string, int>();
+
+            foreach (var kvp in graph)
+            {
+                var node = kvp.Key;
+                var nodeChildren = kvp.Value;
+
+                if (!result.ContainsKey(node))
+                {
+                    result.Add(node, 0);
+                }
+
+                foreach (var child in nodeChildren)
+                {
+                    if (!result.ContainsKey(child))
+                    {
+                        result.Add(child, 1);
+                    }
+                    else
+                    {
+                        result[child]++;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static Dictionary<string, List<string>> ReadGraph(int n)
+        {
+            var result = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine()
+                    .Split(new[] { '-', '>'}, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                var node = line[0].Trim();
+                if (line.Length <= 1)
+                {
+                    result.Add(node, new List<string>());
+                    continue;
+                }
+
+                
+                var childrenNodes = line[1]
+                    .Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                result.Add(node, childrenNodes);
+            }
+
+            return result;
+        }
+    }
+}
