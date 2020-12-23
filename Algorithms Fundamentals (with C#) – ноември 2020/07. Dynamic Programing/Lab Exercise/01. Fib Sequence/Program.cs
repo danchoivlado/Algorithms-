e0f3e4205d855118ac94173e5060c41991b8bf9c
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-23 12:08:40 +0200

    Fib

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/07. Dynamic Programing/Lab Exercise/01. Fib Sequence/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Fib_Sequence
+{
+    class Program
+    {
+        public static Dictionary<int, long> memo;
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            memo = new Dictionary<int, long>();
+
+            Console.WriteLine(Fib(n));
+        }
+
+        private static long Fib(int n)
+        {
+            if (memo.ContainsKey(n))
+            {
+                return memo[n];
+            }
+
+            if(n <=2)
+            {
+                return 1;
+            }
+
+            var res = Fib(n - 1) + Fib(n - 2);
+            memo.Add(n, res);
+            return res;
+        }
+    }
+}
