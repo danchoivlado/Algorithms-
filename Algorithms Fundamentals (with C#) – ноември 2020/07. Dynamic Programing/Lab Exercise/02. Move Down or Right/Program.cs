3535644da26d95c46f6a78552f15cde0cd377ae8
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-23 21:38:22 +0200

    LCS

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/07. Dynamic Programing/Lab Exercise/02. Move Down or Right/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Move_Down_or_Right
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rows = int.Parse(Console.ReadLine());
+            var cols = int.Parse(Console.ReadLine());
+
+            var matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                var line = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+
+            var sumsMatrix = new int[rows, cols];
+
+            sumsMatrix[0, 0] = matrix[0, 0];
+
+            for (int col = 1; col < cols; col++) // right line
+            {
+                var numMatrix = matrix[0, col]; //current number in old matrix
+                var prevNumSums = sumsMatrix[0, col - 1]; //current number in curent matrix
+                sumsMatrix[0, col] = numMatrix + prevNumSums;
+            }
+
+            for (int row = 1; row < rows; row++)
+            {
+                var prevSumMatrix = sumsMatrix[row - 1, 0];
+                var currMatrix = matrix[row, 0];
+                sumsMatrix[row, 0] = prevSumMatrix + currMatrix;
+            }
+
+            for (int row = 1; row < rows; row++)
+            {
+                for (int col = 1; col < cols; col++)
+                {
+                    var right = sumsMatrix[row - 1, col];
+                    var down = sumsMatrix[row, col - 1];
+
+                    var bigger = Math.Max(right, down);
+                    var curMatrix = matrix[row, col];
+                    sumsMatrix[row, col] = bigger + curMatrix;
+                }
+            }
+
+            var curRow = rows - 1;
+            var curCol = cols - 1;
+            var endRes = new Stack<string>();
+            while (curRow != 0 || curCol != 0)
+            {
+                var left = -1;
+                if (curCol - 1 >= 0)
+                {
+                    left = sumsMatrix[curRow, curCol - 1];
+                }
+
+                var up = -1;
+                if (curRow - 1 >= 0)
+                {
+                    up = sumsMatrix[curRow - 1, curCol];
+                }
+
+                if (left >= up)
+                {
+                    endRes.Push($"[{curRow}, {curCol}]");
+                    curCol -= 1;
+                }
+                else
+                {
+                    endRes.Push($"[{curRow}, {curCol }]");
+                    curRow -= 1;
+                }
+            }
+
+            Console.WriteLine($"[0, 0] {string.Join(" ", endRes)}");
+            
+        }
+    }
+}
---
+++ Algorithms Fundamentals (with C#) – ноември 2020/07. Dynamic Programing/Lab Exercise/03. Longest Common/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Longest_Common
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = Console.ReadLine();
+            var second = Console.ReadLine();
+            var lcs = new int[first.Length + 1, second.Length + 1];
+
+            for (int row = 1; row < lcs.GetLength(0); row++)
+            {
+                for (int col = 1; col < lcs.GetLength(1); col++)
+                {
+                    var up = lcs[row - 1, col];
+                    var left = lcs[row, col - 1];
+                    var result = Math.Max(up, left);
+
+                    if (first[row - 1] == second[col - 1])
+                    {
+                        result = Math.Max(lcs[row - 1, col - 1] + 1,result);
+                    }
+
+                    lcs[row, col] = result;
+                }
+            }
+
+            for (int row = 0; row < lcs.GetLength(0); row++)
+            {
+                for (int col = 0; col < lcs.GetLength(1); col++)
+                {
+                    Console.Write(lcs[row, col]);
+                }
+                Console.WriteLine();
+            }
+
+            var curRow = lcs.GetLength(0) - 1;
+            var curCol = lcs.GetLength(1) - 1;
+
+            var endRes = new Stack<char>();
+            while (curCol != 0 && curRow != 0)
+            {
+                var up = -1;
+                if (curRow - 1 >= 0)
+                    up = lcs[curRow - 1, curCol];
+
+                var left = -1;
+                if (curCol - 1 >= 0)
+                    left = lcs[curRow, curCol - 1];
+
+                if (first[curRow - 1] == second[curCol - 1]
+                    && lcs[curRow, curCol] - 1 == lcs[curRow - 1, curCol - 1])
+                {
+                    endRes.Push(first[curRow - 1]);
+                    curCol--;
+                    curRow--;
+                }
+                else if (up >= left)
+                {
+                    curRow--;
+                }
+                else
+                {
+                    curCol--;
+                }
+            }
+            Console.WriteLine(endRes.Count);
+        }
+    }
+}
