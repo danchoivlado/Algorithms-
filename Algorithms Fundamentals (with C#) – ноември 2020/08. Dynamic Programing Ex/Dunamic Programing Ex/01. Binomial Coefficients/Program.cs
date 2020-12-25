26ca3c5bd9f563a3d5fd5a8db7bddb99b5a57e4e
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-25 12:21:24 +0200

    update

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/08. Dynamic Programing Ex/Dunamic Programing Ex/01. Binomial Coefficients/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01._Binomial_Coefficients
+{
+    class Program
+    {
+        public static long[,] cach;
+
+        static void Main(string[] args)
+        {
+            var row = int.Parse(Console.ReadLine());
+            var col = int.Parse(Console.ReadLine());
+
+            cach = new long[row+1, col+1];
+            Console.WriteLine(FindBionomical(row, col)); 
+        }
+
+        private static long FindBionomical(int row, int col)
+        {
+            if(cach[row, col] != 0)
+            {
+                return cach[row, col];
+            }
+
+            if(col == 0 || col == row)
+            {
+                return 1;
+            }
+
+            var result = FindBionomical(row - 1, col - 1) + FindBionomical(row - 1, col);
+            cach[row, col] = result;
+            return result;
+        }
+    }
+}
---
+++ Algorithms Fundamentals (with C#) – ноември 2020/08. Dynamic Programing Ex/Dunamic Programing Ex/02. Dividing Presents/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Dividing_Presents
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            var allSums = CalcSums(nums);
+
+            var sum = nums.Sum();
+            var bobScope = BobScope(allSums, sum / 2);
+            var alanSope = sum - bobScope;
+
+            var alanPresents = new List<int>();
+
+            var cur = alanSope;
+            while (cur != 0)
+            {
+                alanPresents.Add(allSums[cur]);
+                cur -= allSums[cur];
+            }
+
+            Console.WriteLine($"Difference: {bobScope-alanSope}");
+            Console.WriteLine($"Alan:{alanSope} Bob:{bobScope}");
+            Console.WriteLine($"Alan takes: {string.Join(" ", alanPresents)}");
+            Console.WriteLine($"Bob takes the rest.");
+        }
+
+        private static int BobScope(Dictionary<int, int> allSums, int bobScope)
+        {
+            while (true)
+            {
+                if (allSums.ContainsKey(bobScope))
+                {
+                    break;
+                }
+
+                bobScope++;
+            }
+
+            return bobScope;
+        }
+
+        private static Dictionary<int, int> CalcSums(List<int> nums)
+        {
+            var result = new Dictionary<int, int>() { { 0, 0 } };
+
+            foreach (var num in nums)
+            {
+                var sums = result.Keys.ToList();
+
+                foreach (var sum in sums)
+                {
+                    var newSum = sum + num;
+
+                    if (!result.ContainsKey(newSum))
+                    {
+                        result.Add(newSum, num);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
