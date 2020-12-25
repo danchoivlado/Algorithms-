93cc6f8566f01dfdc138fc77856610d49d83de00
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-25 13:26:06 +0200

    Unlimited Coins

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/08. Dynamic Programing Ex/Dunamic Programing Ex/03. Sum with Unlimited/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Sum_with_Unlimited
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coins = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var target = int.Parse(Console.ReadLine());
+
+            var times = CalcTimes(coins, target);
+            Console.WriteLine(times);
+        }
+
+        private static int CalcTimes(List<int> coins, int target)
+        {
+            var memo = new int[target + 1];
+            memo[0] = 1;
+
+            foreach (var coin in coins)
+            {
+                for (int i = coin; i < memo.Length; i++)
+                {
+                    memo[i] = memo[i] + memo[i - coin];
+                }
+            }
+
+            return memo[target];
+        }
+    }
+}
