88bc4a9f7f37d9c966bd7731df108a0ed35ea7ee
Йордан Йорданов <dani_yordani@abv.bg>
2020-12-27 13:53:33 +0200

    up

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/08. Dynamic Programing Ex/Dunamic Programing Ex/06. Connecting Cables/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Connecting_Cables
+{
+    class Program
+    {
+        public static int[] secondRoom;
+        public static int[] firstRoom;
+        public static int[,] matrix;
+
+        static void Main(string[] args)
+        {
+            secondRoom = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var secondLen = secondRoom.Length;
+            firstRoom = new int[secondLen];
+            matrix = new int[secondLen + 1, secondLen + 1];
+
+
+            GenMatrix();
+            Console.WriteLine($"Maximum pairs connected: {matrix[secondLen, secondLen]}");
+        }
+
+        private static void GenMatrix()
+        {
+            for (int i = 0; i < firstRoom.Length; i++)
+            {
+                firstRoom[i] = i + 1;
+            }
+
+            matrix[0, 0] = 0;
+            for (int row = 1; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 1; col < matrix.GetLength(1); col++)
+                {
+                    if (firstRoom[row - 1] == secondRoom[col - 1])
+                    {
+                        matrix[row, col] = matrix[row - 1, col - 1] + 1;
+                    }
+                    else
+                    {
+                        var up = matrix[row - 1, col];
+                        var left = matrix[row, col - 1];
+                        var result = Math.Max(up, left);
+
+                        matrix[row, col] = result;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms Fundamentals (with C#) – ноември 2020/08. Dynamic Programing Ex/Dunamic Programing Ex/07. Minimum Edit Distance/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _07._Minimum_Edit_Distance
+{
+    class Program
+    {
+        public static int[,] matrix;
+        public static int replaceCost;
+        public static int insertCost;
+        public static int deleteCost;
+        public static string firstSt;
+        public static string secondSt;
+
+        static void Main(string[] args)
+        {
+            replaceCost = int.Parse(Console.ReadLine());
+            insertCost = int.Parse(Console.ReadLine());
+            deleteCost = int.Parse(Console.ReadLine());
+            firstSt = Console.ReadLine();
+            secondSt = Console.ReadLine();
+
+            matrix = new int[firstSt.Length + 1, secondSt.Length + 1];
+            GenMatrix();
+            Console.WriteLine($"Minimum edit distance: {matrix[firstSt.Length, secondSt.Length]}") ;
+
+        }
+        private static void GenMatrix()
+        {
+            matrix[0, 0] = 0;
+            for (int col = 1; col < matrix.GetLength(1); col++)
+            {
+                matrix[0, col] = matrix[0, col - 1] + deleteCost;
+            }
+
+            for (int row = 1; row < matrix.GetLength(0); row++)
+            {
+                matrix[row, 0] = matrix[row - 1, 0] + insertCost;
+            }
+        ;
+            for (int row = 1; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 1; col < matrix.GetLength(1); col++)
+                {
+                    var cost = firstSt[row - 1] == secondSt[col - 1] ? 0 : replaceCost;
+
+                    var delete = matrix[row - 1, col] + deleteCost;
+                    var insert = matrix[row, col - 1] + insertCost;
+                    var replace = matrix[row - 1, col - 1] + cost;
+
+                    matrix[row, col] = Math.Min(Math.Min(delete, insert), replace);
+                }
+            }
+        }
+    }
+}
