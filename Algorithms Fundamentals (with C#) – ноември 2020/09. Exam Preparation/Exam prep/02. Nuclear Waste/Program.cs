d9edd27eca05fdd23d2811b5a0ff6e51a976943d
Йордан Йорданов <dani_yordani@abv.bg>
2021-01-02 16:20:26 +0200

    Black Mesa

---
+++ Algorithms Fundamentals (with C#) – ноември 2020/09. Exam Preparation/Exam prep/02. Nuclear Waste/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Nuclear_Waste
+{
+    class Program
+    {
+        public static int[] nums;
+        public static int[] permuts;
+        public static List<int[]> endRes;
+
+        static void Main(string[] args)
+        {
+            nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var slots = int.Parse(Console.ReadLine());
+            permuts = new int[slots];
+            endRes = new List<int[]>();
+            GGen();
+            Console.WriteLine(endRes.Any(x => x.Sum() == 104));
+        }
+
+        private static void GGen(int index = 0)
+        {
+            if(index >= permuts.Length)
+            {
+                endRes.Add(permuts.ToArray());
+                return;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                permuts[index] = nums[i];
+                GGen(index + 1);
+            }
+        }
+    }
+}
---
+++ Algorithms Fundamentals (with C#) – ноември 2020/09. Exam Preparation/Exam prep/03. Black Mesa/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Black_Mesa
+{
+    class Program
+    {
+        public static Dictionary<int, List<int>> graph;
+        public static List<int> path;
+
+        static void Main(string[] args)
+        {
+            var versions = int.Parse(Console.ReadLine());
+            var transitions = int.Parse(Console.ReadLine());
+
+            graph = GenerateGraph(versions, transitions);
+
+            var start = int.Parse(Console.ReadLine());
+            var end = int.Parse(Console.ReadLine());
+
+            var allPath = BFS(start, end);
+            Console.WriteLine(string.Join(" ", path));
+            foreach (var node in graph.Keys)
+            {
+                if (!allPath.Contains(node))
+                {
+                    Console.Write($"{node} ");
+                }
+            }
+
+
+        }
+
+        private static new List<int> BFS(int start, int end)
+        {
+            var queue = new Queue<int>();
+            queue.Enqueue(start);
+            var visited = new HashSet<int>() { start };
+            var endRes = new List<int>();
+
+            while (queue.Count > 0)
+            {
+                var nodeDelete = queue.Dequeue();
+                endRes.Add(nodeDelete);
+                if(nodeDelete == end)
+                {
+                    path = endRes.ToList();
+                }
+                foreach (var child in graph[nodeDelete])
+                {
+                    if (!visited.Contains(child))
+                    {
+                        visited.Add(child);
+                        queue.Enqueue(child);
+                    }
+                }
+            }
+
+            return endRes;
+        }
+
+        private static Dictionary<int, List<int>> GenerateGraph(int versions, int transitions)
+        {
+            var res = new Dictionary<int, List<int>>();
+            for (int i = 1; i <= versions; i++)
+            {
+                res.Add(i, new List<int>());
+            }
+
+            for (int i = 0; i < transitions; i++)
+            {
+                var line = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                var node = line[0];
+                var child = line[1];
+
+                res[node].Add(child);
+            }
+
+            return res;
+        }
+    }
+}
