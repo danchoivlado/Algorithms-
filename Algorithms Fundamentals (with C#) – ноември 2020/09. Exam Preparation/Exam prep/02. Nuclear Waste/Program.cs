91f7a29ab5ebb764bc8b4ac697cc016b9bebf649
Йордан Йорданов <dani_yordani@abv.bg>
2021-01-03 09:10:55 +0200

    update

--- Algorithms Fundamentals (with C#) – ноември 2020/09. Exam Preparation/Exam prep/02. Nuclear Waste/Program.cs
+++ Algorithms Fundamentals (with C#) – ноември 2020/09. Exam Preparation/Exam prep/02. Nuclear Waste/Program.cs
@@ -6,33 +6,33 @@ namespace _02._Nuclear_Waste
 {
     class Program
     {
-        public static int[] nums;
-        public static int[] permuts;
-        public static List<int[]> endRes;
-
         static void Main(string[] args)
         {
-            nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            var slots = int.Parse(Console.ReadLine());
-            permuts = new int[slots];
-            endRes = new List<int[]>();
-            GGen();
-            Console.WriteLine(endRes.Any(x => x.Sum() == 104));
-        }
+            var costs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
 
-        private static void GGen(int index = 0)
-        {
-            if(index >= permuts.Length)
-            {
-                endRes.Add(permuts.ToArray());
-                return;
-            }
+            int[] db = new int[n + 1];
+            int[] prev = new int[n + 1];
 
-            for (int i = 0; i < nums.Length; i++)
+            for (int i = 1; i < n; i++)
             {
-                permuts[index] = nums[i];
-                GGen(index + 1);
+                db[i] = int.MaxValue;
+                for (int j = 1; j <= i; j++)
+                {
+                    if(j > costs.Length)
+                    {
+                        break;
+                    }
+                    int newValue = db[i - j] + costs[j - 1];
+
+                    if(newValue < db[i])
+                    {
+                        db[i] = newValue;
+                        prev[i] = j; 
+                    }
+                }
             }
+            ;
         }
     }
 }
