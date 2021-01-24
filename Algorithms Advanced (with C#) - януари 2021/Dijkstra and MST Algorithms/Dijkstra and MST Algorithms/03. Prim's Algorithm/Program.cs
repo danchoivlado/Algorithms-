b540ef6f8f581b3e4f4de69ed3d8ea714ae4cc17
Йордан Йорданов <dani_yordani@abv.bg>
2021-01-24 19:29:20 +0200

    Prim

---
+++ Algorithms Advanced (with C#) - януари 2021/Dijkstra and MST Algorithms/Dijkstra and MST Algorithms/03. Prim's Algorithm/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+namespace _03._Prim_s_Algorithm
+{
+    class Program
+    {
+        internal class Edge
+        {
+            public Edge(int from, int to, int weight)
+            {
+                this.From = from;
+                this.To = to;
+                this.Weight = weight;
+            }
+            public int From { get; set; }
+
+            public int To { get; set; }
+
+            public int Weight { get; set; }
+        }
+
+        public static Dictionary<int, List<Edge>> graph;
+        public static HashSet<int> forest;
+    
+        static void Main(string[] args)
+        {
+            var numEdges = int.Parse(Console.ReadLine());
+
+
+            graph = ReadGRaph(numEdges);
+            forest = new HashSet<int>();
+
+            foreach (var edge in graph)
+            {
+                if (!forest.Contains(edge.Key))
+                {
+                    startPrim(edge.Key);
+                }
+            }
+        }
+
+        private static void startPrim(int firstNode)
+        {
+            forest.Add(firstNode);
+
+            var queue = new OrderedBag<Edge>(graph[firstNode]
+                , Comparer<Edge>.Create((f, s) => f.Weight - s.Weight));
+
+            while (queue.Count > 0)
+            {
+                var edgeDelete = queue.RemoveFirst();
+                var nodeAdd = GetNode(edgeDelete.From, edgeDelete.To);
+
+                if(nodeAdd == -1)
+                {
+                    continue;
+                }
+
+                forest.Add(nodeAdd);
+                queue.AddMany(graph[nodeAdd]);
+                Console.WriteLine($"{edgeDelete.From} - {edgeDelete.To}");
+            }
+        }
+
+        private static int GetNode(int from, int to)
+        {
+            if (forest.Contains(from) && !forest.Contains(to))
+            {
+                return to;
+            }
+
+            if (forest.Contains(to) && !forest.Contains(from))
+            {
+                return from;
+            }
+
+            return -1;
+        }
+
+        private static Dictionary<int, List<Edge>> ReadGRaph(int numEdges)
+        {
+            var res = new Dictionary<int, List<Edge>>();
+
+            for (int i = 0; i < numEdges; i++)
+            {
+                var line = Console.ReadLine()
+                    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var from = line[0];
+                var to = line[1];
+                var weight = line[2];
+
+                var edge = new Edge(from, to, weight);
+
+                if (!res.ContainsKey(from))
+                {
+                    res[from] = new List<Edge>();
+                }
+
+                if (!res.ContainsKey(to))
+                {
+                    res[to] = new List<Edge>();
+                }
+
+                res[from].Add(edge);
+                res[to].Add(edge);
+            }
+
+            return res;
+        }
+    }
+}
--- Algorithms Advanced (with C#) - януари 2021/Dijkstra and MST Algorithms/Dijkstra and MST Algorithms/Tets/Program.cs
+++ Algorithms Advanced (with C#) - януари 2021/Dijkstra and MST Algorithms/Dijkstra and MST Algorithms/Tets/Program.cs
@@ -1,128 +1,129 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Wintellect.PowerCollections;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Wintellect.PowerCollections;
 
-namespace Tets
-{
-    class Program
+    namespace Tets
     {
-        internal class Edge
+        class Program
         {
-            public int First { get; set; }
-
-            public int Second { get; set; }
-
-            public int Weight { get; set; }
-        }
-
-        public static Dictionary<int, List<Edge>> graph;
-
-        static void Main(string[] args)
-        {
-            var edgesNumber = int.Parse(Console.ReadLine());
-
-            graph = ReadGraph(edgesNumber);
-
-            var start = int.Parse(Console.ReadLine());
-            var end = int.Parse(Console.ReadLine());
-
-            var maxKey = graph.Keys.Max();
-            var distances = new int[maxKey + 1];
-            var prev = new int[maxKey + 1];
-            Fill(distances, int.MaxValue, maxKey+1);
-            distances[start] = 0;
-            prev[0] = -1 ;
-            var queue = new OrderedBag<int>(Comparer<int>.Create((f, s) => distances[f] - distances[s]));
-
-            queue.Add(start);
-
-            while (queue.Count > 0)
+            internal class Edge
             {
-                var minEdge = queue.RemoveFirst();
+                public int First { get; set; }
 
-                if (minEdge == end)
-                {
-                    break;
-                }
+                public int Second { get; set; }
 
-                foreach (var child in graph[minEdge])
+                public int Weight { get; set; }
+            }
+
+            public static Dictionary<int, List<Edge>> graph;
+
+            static void Main(string[] args)
+            {
+                var edgesNumber = int.Parse(Console.ReadLine());
+
+                graph = ReadGraph(edgesNumber);
+
+                var start = int.Parse(Console.ReadLine());
+                var end = int.Parse(Console.ReadLine());
+
+                var maxKey = graph.Keys.Max();
+                var distances = new int[maxKey + 1];
+                var prev = new int[maxKey + 1];
+                Fill(distances, int.MaxValue, maxKey+1);
+                distances[start] = 0;
+                prev[0] = -1 ;
+                var queue = new OrderedBag<int>(Comparer<int>.Create((f, s) => distances[f] - distances[s]));
+
+                queue.Add(start);
+
+                while (queue.Count > 0)
                 {
-                    var actualChild = minEdge == child.First ? child.Second : child.First;
-                    var actualChildWeight = child.Weight + distances[minEdge];
-                    if (distances[actualChild] == int.MaxValue)
+                    var minEdge = queue.RemoveFirst();
+
+                    if (minEdge == end)
                     {
-                        queue.Add(actualChild);
+                        break;
                     }
 
-                    if (distances[actualChild] > actualChildWeight)
+                    foreach (var child in graph[minEdge])
                     {
-                        distances[actualChild] = actualChildWeight;
-                        prev[actualChild] = minEdge;
-                        queue = new OrderedBag<int>(queue, Comparer<int>.Create((f, s) => distances[f] - distances[s]));
+                        var actualChild = minEdge == child.First ? child.Second : child.First;
+                        var actualChildWeight = child.Weight + distances[minEdge];
+                        if (distances[actualChild] == int.MaxValue)
+                        {
+                            queue.Add(actualChild);
+                        }
+
+                        if (distances[actualChild] > actualChildWeight)
+                        {
+                            distances[actualChild] = actualChildWeight;
+                            prev[actualChild] = minEdge;
+                            queue = new OrderedBag<int>(queue, Comparer<int>.Create((f, s) => distances[f] - distances[s]));
+                        }
                     }
                 }
+                Console.WriteLine(distances[end]);
+                Decode(end, prev);
             }
-            Console.WriteLine(distances[end]);
-            Decode(end, prev);
-        }
 
-        private static void Fill(int[] distances, int maxValue,int times)
-        {
-            for (int i = 0; i < times; i++)
+            private static void Fill(int[] distances, int maxValue,int times)
             {
-                distances[i] = int.MaxValue;
+                for (int i = 0; i < times; i++)
+                {
+                    distances[i] = int.MaxValue;
+                }
             }
-        }
 
-        private static void Decode(int end, int[] prev)
-        {
+            private static void Decode(int end, int[] prev)
+            {
 
-            var stack = new Stack<int>();
+                var stack = new Stack<int>();
             
-            var cur = end;
-            while (cur != -1)
-            {
-                stack.Push(cur);
-                cur = prev[cur];
-            }
-            Console.WriteLine(string.Join(" ", stack));
-        }
-
-        private static Dictionary<int, List<Edge>> ReadGraph(int edgesNumber)
-        {
-            var dic = new Dictionary<int, List<Edge>>();
-
-            for (int i = 0; i < edgesNumber; i++)
-            {
-                var line = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-                var start = line[0];
-                var end = line[1];
-                var weight = line[2];
-
-                var edge = new Edge()
+                var cur = end;
+                while (cur != -1)
                 {
-                    First = start,
-                    Second = end,
-                    Weight = weight,
-                };
-
-                if (!dic.ContainsKey(start))
-                {
-                    dic[start] = new List<Edge>();
+                    stack.Push(cur);
+                    cur = prev[cur];
                 }
-
-                if (!dic.ContainsKey(end))
-                {
-                    dic[end] = new List<Edge>();
-                }
-
-                dic[start].Add(edge);
+                Console.WriteLine(string.Join(" ", stack));
             }
 
-            return dic;
+            private static Dictionary<int, List<Edge>> ReadGraph(int edgesNumber)
+            {
+                var dic = new Dictionary<int, List<Edge>>();
+
+                for (int i = 0; i < edgesNumber; i++)
+                {
+                    var line = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                    var start = line[0];
+                    var end = line[1];
+                    var weight = line[2];
+
+                    var edge = new Edge()
+                    {
+                        First = start,
+                        Second = end,
+                        Weight = weight,
+                    };
+
+                    if (!dic.ContainsKey(start))
+                    {
+                        dic[start] = new List<Edge>();
+                    }
+
+                    if (!dic.ContainsKey(end))
+                    {
+                        dic[end] = new List<Edge>();
+                    }
+
+                    dic[start].Add(edge);
+                    dic[end].Add(edge);
+                }
+
+                return dic;
+            }
         }
     }
-}
