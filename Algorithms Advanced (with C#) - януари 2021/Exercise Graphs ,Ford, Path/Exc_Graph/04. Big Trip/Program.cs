b6afb3d7ced37354407b980551930f3efb278e4b
Йордан Йорданов <dani_yordani@abv.bg>
2021-02-11 21:11:46 +0200

    HA

---
+++ Algorithms Advanced (with C#) - януари 2021/Exercise Graphs ,Ford, Path/Exc_Graph/04. Big Trip/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _04._Big_Trip
+{
+    internal class Node
+    {
+        public int From { get; set; }
+
+        public int To { get; set; }
+
+        public int Time { get; set; }
+
+        public Node(int from, int to, int time)
+        {
+            From = from;
+            To = to;
+            Time = time;
+        }
+    }
+
+    class Program
+    {
+        public static List<Node>[] graph;
+        public static Stack<int> topologicalSort;
+
+        static void Main(string[] args)
+        {
+            var numNodes = int.Parse(Console.ReadLine());
+            var numEdges = int.Parse(Console.ReadLine());
+            graph = ReadGraph(numNodes+1, numEdges);
+            var source = int.Parse(Console.ReadLine());
+            var destination = int.Parse(Console.ReadLine());
+
+            topologicalSort = SortTopologicaly();
+
+            var distances = new double[numEdges];
+            Array.Fill(distances,double.NegativeInfinity);
+            distances[source] = 0;
+            var prev = new int[graph.Length];
+            Array.Fill(prev, -1);
+
+            while (topologicalSort.Count > 0)
+            {
+                var curNode = topologicalSort.Pop();
+
+                foreach (var children in graph[curNode])
+                {
+                    var newDistance = distances[curNode] + children.Time;
+                    if (distances[children.To] < newDistance)
+                    {
+                        distances[children.To] = newDistance;
+                        prev[children.To] = curNode;
+                    }
+                }
+            }
+
+            var path = GenRes(prev, destination);
+
+            Console.WriteLine(distances[destination]);
+            Console.WriteLine(string.Join(" ", path));
+        }
+
+        private static Stack<int> SortTopologicaly()
+        {
+            var stack = new Stack<int>();
+            var visited = new bool[graph.Length]; 
+
+            for (int i = 1; i < graph.Length; i++)
+            {
+                DFS(i, visited, stack);
+            }
+
+            return stack;
+        }
+
+        private static void DFS(int node, bool[] visited, Stack<int> sorted)
+        {
+            if (visited[node])
+            {
+                return;
+            }
+
+            visited[node] = true;
+
+            foreach (var child in graph[node])
+            {
+                DFS(child.To, visited, sorted);
+            }
+            sorted.Push(node);
+        }
+
+
+        private static Stack<int> GenRes(int[] prev, int source)
+        {
+            var stack = new Stack<int>();
+            while (source != -1)
+            {
+                stack.Push(source);
+                source = prev[source];
+            }
+
+            return stack;
+        }
+        
+
+        private static List<Node>[] ReadGraph(int numNode, int numEdges)
+        {
+            var res = GenArr(numNode);
+
+            for (int i = 0; i < numEdges; i++)
+            {
+                var line = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                var from = line[0];
+                var to = line[1];
+                var time = line[2];
+                var node = new Node(from, to, time);
+                res[from].Add(node);
+            }
+
+            return res;
+        }
+
+        private static List<Node>[] GenArr(int numNode)
+        {
+            var res = new List<Node>[numNode];
+
+            for (int i = 1; i < numNode; i++)
+            {
+                res[i] = new List<Node>();
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Algorithms Advanced (with C#) - януари 2021/Exercise Graphs ,Ford, Path/Exc_Graph/05. Cable Network/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using Wintellect.PowerCollections;
+
+namespace _05._Cable_Network
+{
+    internal class Node
+    {
+        public int First { get; set; }
+
+        public int Second { get; set; }
+
+        public int Weight { get; set; }
+
+        public Node(int first, int second, int weight)
+        {
+            First = first;
+            Second = second;
+            Weight = weight;
+        }
+    }
+    class Program
+    {
+        public static List<Node>[] graph;
+        public static List<int> spanningTree;
+
+        static void Main(string[] args)
+        {
+            var budget = int.Parse(Console.ReadLine());
+            var numNodes = int.Parse(Console.ReadLine());
+            var numEdges = int.Parse(Console.ReadLine());
+            spanningTree = new List<int>();
+            graph = ReadGraph(numNodes, numEdges);
+
+            var costed = Prim(budget);
+            Console.WriteLine($"Budget used: {costed}");
+        }
+
+        private static int Prim(int budget)
+        {
+            var costedBudget = 0;
+            var queue = new OrderedBag<Node>
+                (Comparer<Node>.Create((f, s) => f.Weight - s.Weight));
+            foreach (var edge in spanningTree)
+            {
+                queue.AddMany(graph[edge]);
+            }
+
+            while (queue.Count > 0)
+            {
+                var curNod = queue.RemoveFirst();
+
+                var actualNode = GetActualNode(curNod);
+
+                if (actualNode == -1)
+                {
+                    continue;
+                }
+
+                if (budget < curNod.Weight)
+                {
+                    break;
+                }
+
+                budget -= curNod.Weight;
+                costedBudget += curNod.Weight;
+
+                spanningTree.Add(actualNode);
+                queue.AddMany(graph[actualNode]);
+            }
+
+            return costedBudget;
+        }
+
+        private static int GetActualNode(Node node)
+        {
+            var an = -1;
+
+            if (spanningTree.Contains(node.First) && !spanningTree.Contains(node.Second))
+            {
+                an = node.Second;
+            }
+            else if (!spanningTree.Contains(node.First) && spanningTree.Contains(node.Second))
+            {
+                an = node.First;
+            }
+
+            return an;
+        }
+
+        private static List<Node>[] ReadGraph(int numNodes, int numEdges)
+        {
+            var res = Fill(numNodes);
+
+            for (int i = 0; i < numEdges; i++)
+            {
+                var line = Console.ReadLine().Split();
+                var first = int.Parse(line[0]);
+                var second = int.Parse(line[1]);
+                var weight = int.Parse(line[2]);
+
+                if (line.Length == 4)
+                {
+                    spanningTree.Add(first);
+                    spanningTree.Add(second);
+                }
+
+                var node = new Node(first, second, weight);
+                var node2 = new Node(second, first, weight);
+                res[first].Add(node);
+                res[second].Add(node2);
+            }
+
+            return res;
+        }
+
+        private static List<Node>[] Fill(int numNodes)
+        {
+            var res = new List<Node>[numNodes];
+            for (int i = 0; i < numNodes; i++)
+            {
+                res[i] = new List<Node>();
+            }
+
+            return res;
+        }
+    }
+}
