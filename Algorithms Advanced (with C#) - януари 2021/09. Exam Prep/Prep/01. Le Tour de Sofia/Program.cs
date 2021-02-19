6435576415042bdaad069363ad63b4879ddbe474
Йордан Йорданов <dani_yordani@abv.bg>
2021-02-19 20:53:01 +0200

    Le Tour de Sofia

---
+++ Algorithms Advanced (with C#) - януари 2021/09. Exam Prep/Prep/01. Le Tour de Sofia/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Runtime.Serialization.Formatters;
+using Wintellect.PowerCollections;
+
+namespace _01._Le_Tour_de_Sofia
+{
+    class Program
+    {
+        public static List<Node>[] graph;
+        public static double[] distances;
+
+        static void Main(string[] args)
+        {
+            var numberJunctions = int.Parse(Console.ReadLine());
+            var numberStreets = int.Parse(Console.ReadLine());
+            var startRoute = int.Parse(Console.ReadLine());
+
+            graph = ReadGraph(numberJunctions, numberStreets);
+
+            distances = new double[numberJunctions];
+
+            for (int i = 0; i < numberJunctions; i++)
+            {
+                distances[i] = double.PositiveInfinity;
+            }
+
+            var queue = new OrderedBag<int>
+                (Comparer<int>.Create((f, s) => distances[f].CompareTo(distances[s])));
+            foreach (var kvp in graph[startRoute])
+            {
+                var child = kvp.To;
+                distances[child] = kvp.Value;
+                queue.Add(child);
+            }
+
+
+            var visited = new HashSet<int>() { startRoute };
+            while (queue.Count > 0)
+            {
+                var node = queue.RemoveFirst();
+                visited.Add(node);
+
+                foreach (var kvp in graph[node])
+                {
+                    var child = kvp.To;
+
+                    if (double.IsPositiveInfinity(distances[child]))
+                    {
+                        queue.Add(child);
+                    }
+
+                    var newDistance = distances[node] + kvp.Value;
+                    if (distances[child] > newDistance)
+                    {
+                        distances[child] = newDistance;
+                        queue = new OrderedBag<int>
+                        (queue, Comparer<int>
+                            .Create((f, s) => distances[f].CompareTo(distances[s])));
+                    }
+                }
+            }
+
+            if (double.IsPositiveInfinity(distances[startRoute]))
+            {
+                Console.WriteLine(visited.Count);
+            }
+            else
+            {
+                Console.WriteLine(distances[startRoute]);
+            }
+        }
+
+        private static List<Node>[] ReadGraph(int numberJunctions, int numberStreets)
+        {
+            var res = new List<Node>[numberJunctions];
+
+            for (int i = 0; i < numberJunctions; i++)
+            {
+                res[i] = new List<Node>();
+            }
+
+            for (int i = 0; i < numberStreets; i++)
+            {
+                var line = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var parent = line[0];
+                var child = line[1];
+                var value = line[2];
+                res[parent].Add(new Node(parent, child, value));
+            }
+
+            return res;
+        }
+    }
+
+    internal class Node 
+    {
+        public int From { get; set; }
+
+        public int To { get; set; }
+
+        public int Value { get; set; }
+
+        public Node(int from, int to, int value)
+        {
+            From = from;
+            To = to;
+            Value = value;
+        }
+    }
+}
--- Algorithms Advanced (with C#) - януари 2021/09. Exam Prep/README.md
+++
@@ -1,7 +0,0 @@
-# Exam Preparation
-- [Video](https://www.youtube.com/watch?v=4E0QWohIE1A "Video")
-- [Task 01](https://drive.google.com/file/d/112OUjrn2qf2slBx6Ny3J_vfv1sX-exF1/view?usp=sharing "Task 1")
-- [Task 02](https://drive.google.com/file/d/1Lc0mRZZy_vZy07HSs1jeRHTI1G9mKH0I/view?usp=sharing "Task 2")
-- [Task 03](https://drive.google.com/file/d/1oVul1RSxYF2wa1GfghOMVWbO_ZM3YtEO/view?usp=sharing "Task 3")
-- [Judge](https://judge.softuni.bg/Contests/Practice/Index/2849#0 "Judge")
-- [SoftUni](https://softuni.bg/trainings/3186/algorithms-advanced-with-c-sharp-january-2021#lesson-19912 "SoftUni")
